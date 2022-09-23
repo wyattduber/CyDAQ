9929c7bb679eb819bde1e597c1373fd07711a26c
Corbeno <corbin@kemsfamily.us>
2022-09-23 15:07:45 -0500

    initial cli script

--- cli/main.py
+++ cli/main.py
@@ -1,1 +1,26 @@
-print("Hello, world!")
+from distutils.cmd import Command
+import os
+import argparse
+
+def ping():
+    print("running ping: TODO implement lol")
+
+def print_help():
+    print("p\t\tping zybo")
+    print("q\t\tquit")
+
+def main():
+    running = True
+    while(running):
+        command = input("> ")
+        if command == 'q':
+            running = False
+        elif command == 'p':
+            ping()
+        else:
+           print_help() 
+
+
+if __name__ == "__main__":
+    # calling the main function
+    main()
