f701db27b37a0e7da16eb35a19c7b753e29ee043
Blake Fisher <blakefisher24@gmail.com>
2022-10-13 16:09:32 -0500

    Button click goes to a hello world function

--- gui/app.py
+++ gui/app.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
+from PyQt5.QtCore import Qt
 
 
 class MainWindow(QMainWindow):
@@ -16,7 +17,12 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(button)
 
     def the_button_was_clicked(self):
-        print("Clicked!")
+        widget = QLabel("Hello World")
+        font = widget.font()
+        font.setPointSize(30)
+        widget.setFont(font)
+        widget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+        self.setCentralWidget(widget)
 
 
 app = QApplication(sys.argv)
