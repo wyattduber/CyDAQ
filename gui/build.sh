aecef80f9ee1a93eaa474052cd0ba0a55054ab22
corbeno <crkems@iastate.edu>
2022-11-14 12:22:54 -0600

    add exe back to build script

--- gui/build.sh
+++ gui/build.sh
@@ -2,12 +2,12 @@
 
 export PATH=$PATH://my.files.iastate.edu/Profiles/$(whoami)/AppData/Roaming/Python/Python38/Scripts
 
-pyuic5 qtdesigner/BasicOperation.ui -o BasicOperation.py
-pyuic5 qtdesigner/MainWindow.ui -o MainWindow.py 
-pyuic5 qtdesigner/DacModeWidget.ui -o DacModeWidget.py
-pyuic5 qtdesigner/SamplingRateWidget.ui -o SamplingRateWidget.py
-pyuic5 qtdesigner/InputWidget.ui -o InputWidget.py
-pyuic5 qtdesigner/FilterWidget.ui -o FilterWidget.py
-pyuic5 qtdesigner/CornersWidget.ui -o CornersWidget.py
+pyuic5.exe qtdesigner/BasicOperation.ui -o BasicOperation.py
+pyuic5.exe qtdesigner/MainWindow.ui -o MainWindow.py 
+pyuic5.exe qtdesigner/DacModeWidget.ui -o DacModeWidget.py
+pyuic5.exe qtdesigner/SamplingRateWidget.ui -o SamplingRateWidget.py
+pyuic5.exe qtdesigner/InputWidget.ui -o InputWidget.py
+pyuic5.exe qtdesigner/FilterWidget.ui -o FilterWidget.py
+pyuic5.exe qtdesigner/CornersWidget.ui -o CornersWidget.py
 
 pyrcc5 qtdesigner/main_window.qrc -o main_window_rc.py
