1a083925ccffafbf99237e6baad28eb4d38c5f0d
Wyatt Duberstein <wyattd@iastate.edu>
2022-11-14 13:19:28 -0600

    finished laying out all the pages

--- gui/main_window_rc.py
+++ gui/main_window_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt5 (Qt v5.9.5)
+# Created by: The Resource Compiler for PyQt5 (Qt v5.15.2)
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -4859,13 +4859,13 @@ qt_resource_struct_v2 = b"\
 \x00\x00\x00\x00\x00\x02\x00\x00\x00\x02\x00\x00\x00\x03\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x3a\x00\x00\x00\x00\x00\x01\x00\x00\x70\x9a\
-\x00\x00\x01\x84\x5d\x8a\xc6\x86\
+\x00\x00\x01\x84\x6c\xda\x2c\x45\
 \x00\x00\x00\x12\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01\x84\x5d\x8a\xc6\x86\
+\x00\x00\x01\x84\x6c\xda\x2c\x4d\
 "
 
-qt_version = QtCore.qVersion().split('.')
-if qt_version < ['5', '8', '0']:
+qt_version = [int(v) for v in QtCore.qVersion().split('.')]
+if qt_version < [5, 8, 0]:
     rcc_version = 1
     qt_resource_struct = qt_resource_struct_v1
 else:
