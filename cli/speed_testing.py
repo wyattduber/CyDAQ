8471d1ade42a0f3014ef14111d30579440806ff1
Wyatt Duberstein <wyattd@iastate.edu>
2023-01-24 15:22:12 -0600

    converted the speed testing into a unit testing class, will test tonight

--- cli/speed_testing.py
+++ cli/speed_testing.py
@@ -1,31 +1,50 @@
 import CLIWrapper
+import unittest
 import time
 
 cli = CLIWrapper.CLI()
 
+class TestTransferSpeeds(unittest.TestCase):
 
-def main():
-    cli.start_sampling()
-    time.sleep(10)
-    start = current_time_millis()
-    cli.stop_sampling()
-    stop = current_time_millis()
-    fancy_date(stop - start)
-
-
-def current_time_millis():
-    return round(time.time() * 1000)
+    # 5 Second Sample Rate Test
+    def five_seconds(self):
+        result = sample(5)
+        fancy_date(result)
+        self.assertTrue(result > 0)
 
 
-def fancy_date(millis):
-    seconds = (millis / 1000) % 60
-    seconds = int(seconds)
-    minutes = (millis / (1000 * 60)) % 60
-    minutes = int(minutes)
-    hours = (millis / (1000 * 60 * 60)) % 24
+    # 10 Second Sample Rate Test
+    def ten_seconds(self):
+        result = sample(5)
+        fancy_date(result)
+        self.assertTrue(result > 0)
 
-    print("Data Transfer took approximately: %d:%d:%d" % (hours, minutes, seconds))
+    
+    # 30 Second Sample Rate Test
+    def thirty_seconds(self):
+        result = sample(5)
+        fancy_date(result)
+        self.assertTrue(result > 0)
+
+    
+    def sample(self, length):
+        cli.start_sampling()
+        time.sleep(length)
+        start = current_time_millis()
+        cli.stop_sampling()
+        stop = current_time_millis()
+        return (stop - start)
+        
+
+    def current_time_millis(self):
+        return round(time.time() * 1000)
 
 
-if __name__ == "__main__":
-    main()
+    def fancy_date(self, millis):
+        seconds = (millis / 1000) % 60
+        seconds = int(seconds)
+        minutes = (millis / (1000 * 60)) % 60
+        minutes = int(minutes)
+        hours = (millis / (1000 * 60 * 60)) % 24
+
+        print("Data Transfer took approximately: %d:%d:%d" % (hours, minutes, seconds))
