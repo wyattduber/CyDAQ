09743396cb3f96a5d6529e2522045a190e7b1c87
Corbeno <corbin@kemsfamily.us>
2023-02-02 02:58:43 -0600

    add serial testing from wednesday night. Got to almost 100million samples per second without writing data and well over 10million when writing

---
+++ serial_testing.py
@@ -0,0 +1,57 @@
+import pty
+import os
+import threading
+import time
+import serial
+
+master,slave = pty.openpty() #open the pseudoterminal
+s_name = os.ttyname(slave) #translate the slave fd to a filename
+
+def listener(port):
+    print("---LISTENER STARTED---")
+    while True:
+        res = b""
+        while not res.endswith(b"!"):
+            res += os.read(port, 1)
+        print("Command recieved: ", res) 
+
+        if res == b'stop!':
+            break
+        elif res == b'@\x04!':
+            os.write(port, b'@')
+            os.write(port, b'\x02\x02'*100_000_000)
+            os.write(port, b'!') 
+        else:
+            print("unknown command")
+
+#create a separate thread that listens on the master device for commands
+thread = threading.Thread(target=listener, args=[master])
+thread.start()
+
+ser = serial.Serial(
+    port=s_name, 
+    baudrate=921600, 
+    timeout=1,
+    bytesize=serial.EIGHTBITS,
+    stopbits=serial.STOPBITS_ONE,
+    xonxoff=False,
+    rtscts=False,
+    dsrdtr=False,
+    parity=serial.PARITY_NONE,
+)
+print(ser)
+
+ser.write(b'@\x04!')
+res = b""
+count = 0
+t0 = time.time()
+with open('hexdatafile.dat', 'wb') as datafile:
+    while not res.endswith(b"!"):
+        res = ser.read_all()
+        datafile.write(res)
+        count+=1
+t1 = time.time()
+print("Time: ", t1-t0)
+print(count)
+
+ser.write(b'stop!')
