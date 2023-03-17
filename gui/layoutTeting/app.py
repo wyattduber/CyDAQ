8a0cb5b5beb987ceb313a968056b3b14742c55a2
Corbeno <corbin@kemsfamily.us>
2023-03-17 00:24:01 -0500

    got a demo working in layoutTeting dir (I spelled it wrong lol)
    Former-commit-id: 7b17222bba648570d37c1b9e0478f84d09acc2ff

---
+++ gui/layoutTeting/app.py
@@ -0,0 +1,45 @@
+import sys
+
+from PyQt5.QtWidgets import *
+from PyQt5 import QtWidgets
+
+from generated.MainWIndow import Ui_MainWindow
+from generated.WidgetA import Ui_WidgetA
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+
+        self.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+
+        self.centerWidget = QtWidgets.QWidget()
+        self.setCentralWidget(self.centerWidget)
+
+        self.widgetA = WidgetA()
+        self.widgetB = QWidget()
+
+        self.stackedLayout = StackedLayout(self.centerWidget)
+        self.stackedLayout.addWidget(self.widgetA)
+        self.stackedLayout.addWidget(self.widgetB)
+
+        self.show()
+
+class StackedLayout(QtWidgets.QStackedLayout):
+    def minimumSize(self):
+        if self.currentWidget():
+            s = self.currentWidget().minimumSize()
+            if s.isEmpty():
+                s = self.currentWidget().minimumSizeHint()
+            return s
+        return super().minimumSize()
+
+class WidgetA(QtWidgets.QWidget, Ui_WidgetA):
+    def __init__(self):
+        super(WidgetA, self).__init__()
+        self.setupUi(self)
+
+
+app = QtWidgets.QApplication(sys.argv)
+main = MainWindow()
+sys.exit(app.exec_())
