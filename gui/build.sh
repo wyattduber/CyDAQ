fb99e99b435c2072a46065ecbb34dd93a339fe86
Corbeno <corbin@kemsfamily.us>
2023-03-18 16:52:06 -0500

    refactor balance beam page to widget
    Former-commit-id: 2994b3e6cf4a6828a3802d8514cd58122c47291c

--- gui/build.sh
+++ gui/build.sh
@@ -3,7 +3,7 @@
 export PATH=$PATH://my.files.iastate.edu/Profiles/$(whoami)/AppData/Roaming/Python/Python38/Scripts
 
 python -m PyQt5.uic.pyuic --from-imports qtdesigner/BasicOperationWidget.ui -o generated/BasicOperationWidgetUI.py
-python -m PyQt5.uic.pyuic --from-imports qtdesigner/BalanceBeam.ui -o generated/BalanceBeamUI.py
+python -m PyQt5.uic.pyuic --from-imports qtdesigner/BalanceBeamWidget.ui -o generated/BalanceBeamWidgetUI.py
 python -m PyQt5.uic.pyuic --from-imports qtdesigner/LiveStreamWidget.ui -o generated/LiveStreamWidgetUI.py
 python -m PyQt5.uic.pyuic --from-imports qtdesigner/DebugWidget.ui -o generated/DebugWidgetUI.py
 python -m PyQt5.uic.pyuic --from-imports qtdesigner/MainWindow.ui -o generated/MainWindowUI.py
